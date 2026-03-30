f481cc647292172bfbc1aaa6f21eea54ede607f6
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-03-30 18:00:46 +0600

    Data fetched successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { Suspense } from 'react'
 import './App.css'
+import Bottles from './components/Bottles/Bottles'
+
+const bottlesPromise = fetch('./bottles.json').then(res => res.json())
 
 function App() {
 
@@ -6,6 +10,9 @@ function App() {
   return (
     <>
       <h1>Buy Awesome Water Bottles</h1>
+      <Suspense fallback={<h3>Bottles are loading...</h3>}>
+        <Bottles bottlesPromise={bottlesPromise}></Bottles>
+      </Suspense>
     </>
   )
 }
