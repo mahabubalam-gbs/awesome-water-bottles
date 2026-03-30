16466276da4ff65b6e8fc8649f0ceabdcdb86c6b
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-03-30 22:12:20 +0600

    Bottles information are added

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './Bottle.css'
 
 const Bottle = ({ bottle }) => {
-    const { img } = bottle;
+    const { img, name, price, stock } = bottle;
     console.log(bottle)
     return (
         <div className='card bottle'>
             <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>${price}</p>
+            <p>Stock Available: {stock}</p>
+            <button className='button'>Buy Now</button>
         </div>
     );
 };
