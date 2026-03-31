8d62db3e8b3e48ab147aec70062113b6d3604e6a
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-03-31 23:58:22 +0600

    Cart bottles images resized

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
 
     return (
-        <div>
+        <div className='cart-container'>
             {
                 cart.map(bottle => <div key={bottle.id}>
                     <img src={bottle.img} alt="" />
